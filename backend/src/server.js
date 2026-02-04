97e8a321e64068f43d1ea9cc18a14d21d9ce311e
monthitra <bussara23110@gmail.com>
2026-02-04 07:47:33 +0700

    FINAL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,18 @@
 import app from "./app.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "../../frontend")));
+
+// route fallback
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
