a3652be319cdccd2889db2f911af063b2267c341
monthitra <bussara23110@gmail.com>
2026-02-10 09:32:29 +0700

    Final

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,7 +39,7 @@ app.use("/api/promos", promoRoutes);
 
 // SPA fallback
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../public/user.html"));
+  res.sendFile(path.join(__dirname, "../frontend/user.html"));
 });
 
 export default app;
