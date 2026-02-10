4ca38f73408aa2c055fc50183e5eb5d96b3e9a0c
monthitra <bussara23110@gmail.com>
2026-02-10 14:10:53 +0700

    Final

---
+++ backend/src/services/user.admin.service.js
@@ -0,0 +1,23 @@
+import db from "../config/db.js";
+
+/* ===============================
+   GET ALL USERS
+================================ */
+export const getAll = async () => {
+  const [rows] = await db.query(`
+    SELECT id, username, email, role, created_at
+    FROM users
+    ORDER BY created_at DESC
+  `);
+  return rows;
+};
+
+/* ===============================
+   DELETE USER
+================================ */
+export const remove = async (id) => {
+  await db.query(
+    "DELETE FROM users WHERE id = ?",
+    [id]
+  );
+};
